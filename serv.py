587df244b018a1933a0df1cf9f36ed49b20e1197
Name <e-mail>
2016-02-25 13:53:23 +0000

    fix

--- serv.py
+++ serv.py
@@ -3,22 +3,22 @@ import asyncore,socket
 host, port = '0.0.0.0',2222
 
 class EchoHandler(asyncore.dispatcher_with_send):
-    def handle_read(self):
-         if data:
-            self.send(data)
-            if data == 'close':
-                self.close()
+	def handle_read(self):
+		if data =='close':
+			self.close()
+		elif data:
+			self.send(data)
 class EchoServer(asyncore.dispatcher):
-    def __init__(self, host, port):
-        asyncore.dispatcher.__init__(self)
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.set_reuse_addr()
-        self.bind((host, port))
-        self.listen(15)
-    def handle_accept(self):
-        pair = self.accept()
-        if pair is not None:
-            sock, addr = pair
-            handler = EchoHandler(sock)
+	def __init__(self, host, port):
+		asyncore.dispatcher.__init__(self)
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.set_reuse_addr()
+		self.bind((host, port))
+		self.listen(15)
+	def handle_accept(self):
+		pair = self.accept()
+		if pair is not None:
+			sock, addr = pair
+			handler = EchoHandler(sock)
 server = EchoServer(host, port)
 asyncore.loop()
